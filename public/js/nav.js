1303d5a2a568ff6ce56b9ccb6b2f0f429399e435
silver <eungi429@gmail.com>
2023-05-18 11:51:05 +0900

    petitcoat - footer finish, nav format

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,15 +1,59 @@
-const navToggle = document.querySelector(".nav-toggle");
-const dropdownMenu = document.querySelector(".dropdown-menu");
+const mobNav = document.querySelector(".nav-toggle");
+const mobDd = document.querySelector(".mob-dd");
+const shopDd = document.querySelector(".shop-dd");
+const aboutDd = document.querySelector(".about-dd");
 
-navToggle.addEventListener("click", function () {
-  dropdownMenu.classList.toggle("open");
-  checkDropDown();
+const shopNav = document.querySelector(".shop-nav");
+const aboutNav = document.querySelector(".about-nav");
+const loginNav = document.querySelector(".login-nav");
+const cartNav = document.querySelector(".cart-nav");
+
+const toggleList = [mobNav, shopNav, aboutNav, loginNav, cartNav];
+const shopNavList = ["NEW ARRIVAL", "ALL", "PHONE CASE", "GRIPTOK", "SELECT"];
+const aboutNavList = ["BRAND STORY", "LOOKBOOK", "SHOWROOM", "CUSTOMER CARE"];
+
+const updateHtml = (navList) => {
+  return navList
+    .map((item) => {
+      return `<li><a href="/${item.toLowerCase()}" class="nav-link">${item}</a></li>`;
+    })
+    .join("");
+};
+
+toggleList.map((item) => {
+  item.addEventListener("click", () => {
+    switch (item) {
+      case mobNav:
+        mobDd.classList.toggle("open");
+        break;
+      case shopNav:
+        const ulEl = document.querySelector(".shop-dd .dd-ul");
+        ulEl.innerHTML = updateHtml(shopNavList);
+        shopDd.classList.toggle("open");
+        break;
+      case aboutNav:
+        const abulEl = document.querySelector(".about-dd .dd-ul");
+        abulEl.innerHTML = updateHtml(aboutNavList);
+        aboutDd.classList.toggle("open");
+        break;
+      default:
+        break;
+    }
+  });
 });
 
-const checkDropDown = () => {
-  if (dropdownMenu.classList.contains("open")) {
-    console.log("dropdown is open");
-  } else {
-    console.log("dropdwon is closed");
+document.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (!mobDd.contains(target) && !mobNav.contains(target)) {
+    mobDd.classList.remove("open");
   }
-};
+
+  if (!shopDd.contains(target) && !shopNav.contains(target)) {
+    shopDd.classList.remove("open");
+  }
+
+  if (!aboutDd.contains(target) && !aboutNav.contains(target)) {
+    aboutDd.classList.remove("open");
+  }
+});
